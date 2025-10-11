b29aab6374c301b81553a484e8d9f601b55b6c0b
caocchinh <chinhcaocu@gmail.com>
2025-10-11 12:05:56 +0700

    fix(SpreadSheetQuickAccess, constants): replace 'id' with 'href' for unique key generation in spreadsheet links

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -296,32 +296,26 @@ export const SILENCIO_HAUNTED_HOUSE_QUEUE_MANAGER_URL =
 // Spreadsheet & back-up Links
 export const SPREADSHEET_LINKS = [
   {
-    id: "offline-sales",
     label: "Sheet bán vé offline",
     href: `https://docs.google.com/spreadsheets/d/${OFFLINE_SALES_SHEET_ID}`,
   },
   {
-    id: "online-sales",
     label: "Sheet bán vé online",
     href: `https://docs.google.com/spreadsheets/d/${ONLINE_SALES_SHEET_ID}`,
   },
   {
-    id: "checkin",
     label: "Sheet check-in",
     href: `https://docs.google.com/spreadsheets/d/${CHECKIN_SHEET_ID}`,
   },
   {
-    id: "teacher-verification",
     label: "Sheet xác nhận GVCN",
     href: `https://docs.google.com/spreadsheets/d/${TEACHER_VERIFICATION_SHEET_ID}`,
   },
   {
-    id: "backup-form",
     label: "Form bán vé dự phòng",
     href: `https://docs.google.com/forms/d/e/1FAIpQLSfpRH5kU4lqvTnwgs_lmOHtYjo_RqZ2EDGlS6d7hewavX6zBg/viewform?usp=sharing&ouid=103069438122563507136`,
   },
   {
-    id: "backup-sheet",
     label: "Sheet bán vé dự phòng",
     href: `https://docs.google.com/spreadsheets/d/1jV3n7OyCSe5WSVTNqBtb9csoIgye1-ue7VdntDPCzrY/edit?usp=sharing`,
   },
